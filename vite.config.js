6f3d86628f11aa49d02e0f76ece1ad1972c54e61
Majid Feiz <sermajid3@yahoo.com>
2025-12-30 02:24:19 +0330

    bugfix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,14 @@ export default defineConfig({
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
-  }
+  },
+  resolve: {
+    alias: {
+      // CKEditor bare specifier fix (otherwise browser sees an unresolved bare import)
+      '@ckeditor/ckeditor5-watchdog': path.resolve(__dirname, 'node_modules/@ckeditor/ckeditor5-watchdog/dist/index.js'),
+    },
+  },
+  optimizeDeps: {
+    include: ['@ckeditor/ckeditor5-watchdog'],
+  },
 })
